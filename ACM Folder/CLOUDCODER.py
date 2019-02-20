a7c11c6658de075aeed1d6863df5bde2877d0946
JoeMusco <jmuscolino@email.arizona.edu>
2019-02-19 17:39:07 -0700

    first commit

---
+++ ACM Folder/CLOUDCODER.py
@@ -0,0 +1,20 @@
+def prac(lis):
+    for i in range(0, len(lis)):
+        pass
+    print(i)
+        
+        
+        
+    
+    
+
+
+
+def main():
+    lis = ['aa', 'bb', 'cc','dd']
+    start = 1
+    stop = 3
+    prac(lis)
+
+
+main()
